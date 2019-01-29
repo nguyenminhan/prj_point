49b2ed9f31e504c87ae7f036d18bb81699e76a39
minhan <anlaptrinhweb97@gmail.com>
2019-01-29 20:39:56 +0700

    fix theme

--- resources/views/point/index.blade.php
+++ resources/views/point/index.blade.php
@@ -4,25 +4,20 @@
     <div class="row" style="margin-top: 100px;">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-primary">
-                <div class="panel-heading text-center" style="font-size: 25px;">Point後付けWebアプリ</div>
-                <div class="panel-body">
-                    {{-- <form action="/k"> --}}
+                <div class="panel-heading text-center" style="font-size: 25px;">会員ポイント付与アプリ</div>
+                    <div class="panel-body">   
                         <div class="form-group">
                             <label for="customer_code"><b>会員番号：</b></label>          
-                            <input type="text" class="form-control" name="customer_code" id="customer_code" placeholder="customerCode"   style="height: 45px;">
+                            <input type="text" class="form-control" name="customer_code" id="customer_code" placeholder="customerCode"   style="height: 60px;">
                         </div>
                         <div class="form-group"> 
                             <label for="customer_code"><b>レシート：</b></label>                             
-                            <input type="text" class="form-control" name="transactionUuid" id="transactionUuid" placeholder="transactionUuid" style="height: 45px;">
+                            <input type="text" class="form-control" name="transactionUuid" id="transactionUuid" placeholder="transactionUuid" style="height: 60px;">
                         </div>
                         <div class="form-group"> 
-                            <div class="col-sm-offset-7 col-sm-9">
-                                <button type="button" id="ok" class="btn btn-primary">OK</button>
+                            <button style="margin: 0px auto;width: 100%;display: block;height: 60px;" type="button" id="ok" class="btn btn-primary">送信する</button>
                         </div>
-                    {{-- </form> --}}
-                            {{-- </div> --}}
-                        {{-- </div> --}}
-                    {{-- </form> --}}
+                    </div>
                 </div>
             </div>
         </div>
