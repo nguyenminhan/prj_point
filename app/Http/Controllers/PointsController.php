ae2692b1b7ba474402358e9caad7d171c32e9ccb
minhan <nguyenminhan0402@gmail.com>
2019-02-25 12:57:20 +0700

    update ngay

--- app/Http/Controllers/PointsController.php
+++ app/Http/Controllers/PointsController.php
@@ -40,7 +40,10 @@ class PointsController extends Controller
         $item = [
     		[
                 "transactionUuid" => $data_all['transactionUuid'],
-                "transactionHeadDivision" => 1
+                "transactionHeadDivision" => "1",
+                "storeId <>" => "1",
+                "storeId <>" => "39",
+                "storeId <>" => "40"
             ]
     	];
     	$params = [
@@ -74,7 +77,7 @@ class PointsController extends Controller
     	];
 
     	$params1 = [
-            "fields" => ["customerCode","lastName","firstName","customerId","rank","point"],
+            "fields" => ["customerCode","lastName","firstName","customerId","rank","point","customerNo","sex","pointGivingUnitPrice","pointGivingUnit"],
             "conditions" => $item1 ,
             "order" => ["customerCode desc"],
             "table_name" => "Customer"
@@ -95,6 +98,13 @@ class PointsController extends Controller
         }else{
             $get_customer = $rs1['result'][0]['customerCode'];
             $point_current =  $rs1['result'][0]['point'];
+            $customerId = $rs1['result'][0]['customerId'];
+            $lastName = $rs1['result'][0]['lastName'];
+            $firstName = $rs1['result'][0]['firstName'];
+            $sex = $rs1['result'][0]['sex'];
+            $pointGivingUnitPrice = $rs1['result'][0]['pointGivingUnitPrice'];
+            $pointGivingUnit = $rs1['result'][0]['pointGivingUnit'];
+            $customerNo =  $rs1['result'][0]['customerNo'];
         }       
 
 
@@ -104,37 +114,58 @@ class PointsController extends Controller
         
 
     	// goi api tra ve price
-    	$price_api = 0;
-        $check1 = false;
-    	foreach ($get_point  as  $value) {
+    	   $price_api = 0;
+        // $check1 = false;
+    	// foreach ($get_point  as  $value) {
+        
             // check receipt quá 3 tháng thì k được update point
             // echo $date_current."<br>";die;
-            $date_api = $value['transactionDateTime'];
+            $date_api = $get_point[0]['transactionDateTime'];
+
             $date_api = strtotime($date_api);
                 // $date_api = date('Y-m-d H:i:s', $date_api);
             $date_api = mktime(date("H",$date_api), date("i",$date_api), date("s",$date_api), date("m", $date_api)+3, date("d", $date_api),   date("Y", $date_api));
             $date_current = date('Y-m-d H:i:s');
             $date_current = strtotime($date_current);
             if($date_api >= $date_current){
-                if($get_customer == $value['customerCode']) {
+                if($get_customer == $get_point[0]['customerCode']) {
                     return json_encode(array(
                         'error_code'  => 6,
                         'error_msg' => 'すでにこのレシートは登録されています。'
                     ));
-                    }
-                $price_api += $value['subtotal'];
-                $check1 = true;
-                break;    
+                }else{
+                    $price_api +=  $get_point[0]['subtotal'];
+                    // cộng thêm 1 năm để update
+                    $terminalTranDateTime_api =  $get_point[0]['terminalTranDateTime'];
+                    $terminalTranDateTime_api = strtotime($terminalTranDateTime_api);
+                    $terminalTranDateTime_api = mktime(date("H",$terminalTranDateTime_api), date("i",$terminalTranDateTime_api), date("s",$terminalTranDateTime_api), date("m", $terminalTranDateTime_api), date("d", $terminalTranDateTime_api),   date("Y", $terminalTranDateTime_api)+1);
+                    $terminalTranDateTime_api = date('Y-m-d', $terminalTranDateTime_api);
+
+                    $this->updateCustomer($customerId,$get_customer,$customerNo,$lastName,$firstName,$sex,$pointGivingUnitPrice, $pointGivingUnit,$terminalTranDateTime_api);
+                }
+            }else{
+                return json_encode(array(
+                    'error_code'  => 9,
+                    'error_msg' => 'レシートの期限は三か月までです。このレシートは期限切れになりました。'
+                ));
+                exit();
             }
+              
+            
+
+                // $price_api += $value['subtotal'];
+                // $check1 = true;
+                // break;    
+            // }
             	
-    	}
-        if(!$check1){
-            return json_encode(array(
-                'error_code'  => 9,
-                'error_msg' => 'レシートの期限は三か月までです。このレシートは期限切れになりました。'
-            ));
-            exit();
-        }
+    	// }
+        // if(!$check1){
+        //     return json_encode(array(
+        //         'error_code'  => 9,
+        //         'error_msg' => 'レシートの期限は三か月までです。このレシートは期限切れになりました。'
+        //     ));
+        //     exit();
+        // }
     	$jsonRank = file_get_contents(base_path('resources/lang/rank.json'));
     	$jsonRank = json_decode($jsonRank, true);
     	$rank = $rs1['result'][0]['rank'];
@@ -145,6 +176,10 @@ class PointsController extends Controller
                 if($price_api > 0){
                     $point_new += (int)($price_api/100) * $value['point'];
                 }
+                // if($get_point[0]['subtotal'] > 0){
+                //     $point_new += (int)($get_point[0]['subtotal'] /100 ) * $value['point'];
+                // }
+                
     			$check3 = true;
     			break;
     		}
@@ -173,7 +208,7 @@ class PointsController extends Controller
 
             DB::table(env('MODEL_TABLE'))->insert($data_point);
 
-            $update_point = $this->updatePoint($point_total,$rs1['result'][0]['customerId']);
+            $update_point = $this->updatePoint($point_total,$customerId);
             // return $update_point;
             return json_encode(array(
                 'error_code'  => 0,
@@ -238,6 +273,43 @@ class PointsController extends Controller
          $this->pointapi->getApi($point_update);
         // return $rs;
         return $point;
+    }
+
+    public function updateCustomer($customer_id,$customerCode,$customerNo,$lastName,$firstName,$sex,$pointGivingUnitPrice,$pointGivingUnit,$pointExpireDate){
+        $proc_info = [
+                "proc_division" => "U"
+        ];
+        $row = [
+            [
+                "customerId" => $customer_id,
+                "customerCode" => $customerCode,
+                "customerNo" =>$customerNo ,
+                "lastName" =>$lastName ,
+                "firstName" =>$firstName ,
+                "sex" => $sex,
+                "pointGivingUnitPrice" =>$pointGivingUnitPrice ,
+                "pointGivingUnit" =>$pointGivingUnit ,
+                "pointExpireDate" => $pointExpireDate
+            ]
+        ];
+        $data = [
+            [
+                "table_name" => "Customer",
+                "rows" => $row
+            ]
+        ];
+
+        $params = [
+                "proc_info" => $proc_info,
+                "data" => $data
+            ];
+        $customer_update = [
+            "proc_name" => "customer_upd",
+            "params" => json_encode($params,true)
+        ];          
+        $rs = $this->pointapi->getApi($customer_update);
+        return $rs;
+        // return $pointExpireDate;
     }
     
 }
